33547bf97291dce458b0244d5bb8b148e1d69435
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2023-04-07 18:22:50 -0300

    Relacionamentos e migration

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -6,10 +6,20 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
-
         public Seller? Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller? seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
     }
 }
