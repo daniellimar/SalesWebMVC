81ba56830a1494edac314948281b73d886f3b670
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2023-04-07 17:42:56 -0300

    Models Seller e SalesRecord, Enum: SaleStatus

---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,13 @@
+﻿using SalesWebMVC.Models.Enums;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+    }
+}
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+    }
+}
